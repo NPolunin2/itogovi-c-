1861602cb3b4bc35c2ffd50b176824b92b9e68a0
Nikita <nikm9@mail.ru>
2023-01-03 12:59:39 +1100

    done task1

---
+++ dz/task1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+
+void PrintInterval(int m, int n)
+{
+    int start = m;
+    int end = n;
+    if (m > n)
+    {
+        start = n;
+        end = m;
+    }
+    for (int i = start; i <= end; i++)
+    {
+        Console.Write($"{i} ");
+    }
+}
+PrintInterval(22, 9);
+
+Console.WriteLine();
